692516000d73ed15053bb277db4a87820698952f
Calla Rudolph and Nicole Freed <calla+nicole@>
2017-09-25 14:25:48 -0700

    add RECEIVE_SONG to action types

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -2,3 +2,4 @@ export const NEXT_LINE = "NEXT_LINE";
 export const RESTART_SONG = "RESTART_SONG";
 export const SELECT_SONG = "SELECT_SONG";
 export const REQUEST_SONG = "REQUEST_SONG";
+export const RECEIVE_SONG = "RECEIVE_SONG";
