6b02f6480f53ff1ea5548738bf8c45739d30de1c
Calla Rudolph and Nicole Freed <nicole@>
2017-09-25 10:21:38 -0700

    refactor SongSearch into class-based component

--- src/components/SongSearch.jsx
+++ src/components/SongSearch.jsx
@@ -3,31 +3,33 @@ import styles from "./../styles/SongSearch.css";
 import { fetchSongId } from "./../actions";
 import { connect } from "react-redux";
 
-function SongSearch(){
-  let input;
+class SongSearch extends React.Component {
 
-  return (
-    <div className = {styles.songSearch}>
-      <form onSubmit={e => {
-        e.preventDefault();
-        if (!input.value.trim()) {
-          return;
-        }
+  render() {
+    let input;
+    return (
+      <div className = {styles.songSearch}>
+        <form onSubmit={e => {
+          e.preventDefault();
+          if (!input.value.trim()) {
+            return;
+          }
 
-        // this is where custom logic goes
-        console.log("SEARCHED SONG:");
-        console.log(input.value.trim());
-        //
+          // this is where custom logic goes
+          console.log("SEARCHED SONG:");
+          console.log(input.value.trim());
+          //
 
-        input.value = "";
-      }}>
-        <input placeholder="Song Title" ref={node => {
-          input = node;
-        }}></input>
-        <button>search</button>
-      </form>
-    </div>
-  );
+          input.value = "";
+        }}>
+          <input placeholder="Song Title" ref={node => {
+            input = node;
+          }}></input>
+          <button>search</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default SongSearch;
+export default connect()(SongSearch);
