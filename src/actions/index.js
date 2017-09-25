9aafb8d3dfbcbdb1193f9fee986b7778d455aefb
Calla Rudolph and Nicole Freed <calla+nicole@>
2017-09-25 14:37:28 -0700

    dispatch selectSong in fetchLyrics

--- src/actions/index.js
+++ src/actions/index.js
@@ -46,6 +46,7 @@ export function fetchLyrics(title, artist, musicMatchId, localSongId, dispatch) 
       lyrics = lyrics.replace('"', "");
       const songArray = lyrics.split(/\n/g);
       dispatch(receiveSong(title, artist, localSongId, songArray));
+      dispatch(selectSong(localSongId));
     } else {
       console.log("We couldn't locate lyrics for this song!")
     }
