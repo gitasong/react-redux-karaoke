5ebf64def893a44a3cd7a7a3811b7bc63b8fb06b
Calla Rudolph and Nicole Freed <calla@>
2017-09-25 10:42:14 -0700

    import applyMiddleware, thunkMiddleware, createLogger

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import styles from "./styles/styles.css";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
 
 const store = createStore(reducer);
 
