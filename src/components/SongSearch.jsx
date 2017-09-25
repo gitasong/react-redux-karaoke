518e79d73a03da1572acf278467b519695a3a3ec
Calla Rudolph and Nicole Freed <calla@>
2017-09-25 10:19:26 -0700

    import fetchSongId and connect

--- src/components/SongSearch.jsx
+++ src/components/SongSearch.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "./../styles/SongSearch.css";
+import { fetchSongId } from "./../actions";
+import { connect } from "react-redux";
 
 function SongSearch(){
   let input;
